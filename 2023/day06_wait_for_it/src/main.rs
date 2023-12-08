68d5d669fc642fb1c66c6e0d26d23b1676012a34
Joshua Letcher <caffeinatedcm@gmail.com>
2023-12-07 19:01:38 -0600

    2023 day 6 part 2

--- 2023/day06_wait_for_it/src/main.rs
+++ 2023/day06_wait_for_it/src/main.rs
@@ -3,21 +3,25 @@ fn main() {
 
     let races = parse_input(input);
 
-    let result : i32 = races.iter().map(|x| count_win_options(x)).product();
+    let result : i64 = races.iter().map(|x| count_win_options(x)).product();
 
     println!("Result: {}", result);
+
+    let combined_race = parse_input2(input);
+
+    println!("Combined race result: {}", count_win_options(&combined_race));
 }
 
-fn parse_input_line(input: &str) -> Vec<i32> {
-    input.split_whitespace().skip(1).map(|s| s.parse::<i32>().unwrap()).collect::<Vec<i32>>()
+fn parse_input_line(input: &str) -> Vec<i64> {
+    input.split_whitespace().skip(1).map(|s| s.parse::<i64>().unwrap()).collect::<Vec<i64>>()
 }
 
-fn parse_input(input: &str) -> Vec<(i32, i32)> {
+fn parse_input(input: &str) -> Vec<(i64, i64)> {
     let mut lines = input.lines();
     let times = parse_input_line(lines.next().unwrap());
     let distance = parse_input_line(lines.next().unwrap());
 
-    times.into_iter().zip(distance.into_iter()).collect::<Vec<(i32, i32)>>()
+    times.into_iter().zip(distance.into_iter()).collect::<Vec<(i64, i64)>>()
 }
 
 #[test]
@@ -27,7 +31,22 @@ fn test_parse_input() {
     assert_eq!(result, vec![(7, 9), (15, 40), (30, 200)]);
 }
 
-fn count_win_options(race: &(i32, i32)) -> i32 {
+fn parse_input2(input: &str) -> (i64, i64) {
+    let mut lines = input.lines();
+    let times = parse_input_line(lines.next().unwrap()).iter().fold(String::new(), |acc, x| acc + x.to_string().as_str());
+    let distance = parse_input_line(lines.next().unwrap()).iter().fold(String::new(), |acc, x| acc + x.to_string().as_str());
+
+    (times.parse::<i64>().unwrap(), distance.parse::<i64>().unwrap())
+}
+
+#[test]
+fn test_parse_input2() {
+    let input = include_str!("./example1.txt");
+    let result = parse_input2(input);
+    assert_eq!(result, (71530, 940200));
+}
+
+fn count_win_options(race: &(i64, i64)) -> i64 {
     let mut win_options = 0;
     for t in 1..race.0 {
         let speed = t;
