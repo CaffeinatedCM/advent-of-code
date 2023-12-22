2ed9dbd6ae69274f2c79413840459af1e4188ad9
Josh Letcher <josh.letcher@skyspecs.com>
2023-12-22 10:55:55 -0600

    wip 2023 day 21 part 2, still too slow

--- 2023/day_21_step_counter/src/main.rs
+++ 2023/day_21_step_counter/src/main.rs
@@ -1,15 +1,29 @@
+use rayon::prelude::*;
 use std::collections::HashSet;
+use std::sync::{Arc, Mutex};
 
 fn main() {
     let input = include_str!("./input.txt");
     let map = parse_input(input);
 
-    println!("Options: {}", find_possible_points(&map.map, map.start, 64));
+    println!("Options: {}", find_possible_points(&map, 64));
+    println!("Options OH MY: {}", find_possible_points(&map, 26501365));
 }
 
 struct Map {
     map: Vec<Vec<char>>,
-    start: (i32, i32),
+    start: (i64, i64),
+}
+
+impl Map {
+    fn at(&self, x: i64, y: i64) -> char {
+        let map_width = self.map[0].len() as i64;
+        let map_height = self.map.len() as i64;
+
+        let virtual_x = (x % map_width + map_width) % map_width;
+        let virtual_y = (y % map_height + map_height) % map_height;
+        self.map[virtual_y as usize][virtual_x as usize]
+    }
 }
 
 fn parse_input(input: &str) -> Map {
@@ -21,7 +35,7 @@ fn parse_input(input: &str) -> Map {
         for (x, c) in line.chars().enumerate() {
             row.push(c);
             if c == 'S' {
-                start = (x as i32, y as i32);
+                start = (x as i64, y as i64);
             }
         }
         map.push(row);
@@ -30,52 +44,23 @@ fn parse_input(input: &str) -> Map {
     Map { map, start }
 }
 
-fn find_possible_points(map: &Vec<Vec<char>>, position: (i32, i32), steps: i32) -> i32 {
-    let (x, y) = position;
-
-    if x < 0
-        || x >= map.len() as i32
-        || y < 0
-        || y >= map[0].len() as i32
-        || map[y as usize][x as usize] == '#'
-    {
-        return 0;
-    }
-
-    if steps == 0 {
-        return 1;
-    }
+fn find_possible_points(map: &Map, steps: i64) -> i64 {
+    let (x, y) = map.start;
 
     let mut points = HashSet::new();
     points.insert((x, y));
     for _ in 0..steps {
         let mut next_points = HashSet::new();
-        for point in points {
-            let (x, y) = point;
-
-            if x < 0
-                || x >= map[0].len() as i32
-                || y < 0
-                || y >= map.len() as i32
-                || map[y as usize][x as usize] == '#'
-            {
-                continue;
-            }
-
-            next_points.insert((x + 1, y));
-            next_points.insert((x - 1, y));
-            next_points.insert((x, y + 1));
-            next_points.insert((x, y - 1));
-        }
+        next_points.par_extend(
+            points
+                .par_iter()
+                .filter(|(x, y)| map.at(*x, *y) != '#')
+                .flat_map(|(x, y)| vec![(x + 1, *y), (x - 1, *y), (*x, y + 1), (*x, y - 1)]),
+        );
         points = next_points;
     }
 
-    points
-        .iter()
-        .filter(|(x, y)| x >= &0 && y >= &0)
-        .filter(|(x, y)| x < &(map[0].len() as i32) && y < &(map.len() as i32))
-        .filter(|(x, y)| map[*y as usize][*x as usize] != '#')
-        .count() as i32
+    points.iter().filter(|(x, y)| map.at(*x, *y) != '#').count() as i64
 }
 
 #[test]
@@ -83,5 +68,8 @@ fn test_find_possible_points() {
     let input = include_str!("./example1.txt");
     let map = parse_input(input);
 
-    assert_eq!(find_possible_points(&map.map, map.start, 6), 16);
+    assert_eq!(find_possible_points(&map, 6), 16);
+    assert_eq!(find_possible_points(&map, 50), 1594);
+    assert_eq!(find_possible_points(&map, 500), 167004);
+    assert_eq!(find_possible_points(&map, 5000), 16733044);
 }
