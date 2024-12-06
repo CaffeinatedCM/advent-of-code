44729d906afecf91e29978b7163386b78dea1a96
Josh <josh.letcher@skyspecs.com>
2024-12-06 10:35:26 -0600

    2025 day 6 part 2

--- 2024/day_06_guard_gallivant/src/main.rs
+++ 2024/day_06_guard_gallivant/src/main.rs
@@ -1,3 +1,5 @@
+use std::collections::HashSet;
+
 fn main() {
     let args = std::env::args().collect::<Vec<String>>();
 
@@ -9,8 +11,11 @@ fn main() {
     let input = std::fs::read_to_string(&args[1]).unwrap();
     let map = parse_input(&input);
 
-    let positions = count_positions(&map);
-    println!("Positions: {}", positions);
+    let positions = find_patrol_positions(&map);
+    println!("Position count: {}", positions.len());
+
+    let possible_loops = count_possible_loops(&map, &positions);
+    println!("Possible loops: {}", possible_loops);
 }
 
 fn parse_input(input: &str) -> Vec<Vec<char>> {
@@ -29,18 +34,13 @@ fn get_start_pos(map: &Vec<Vec<char>>) -> (usize, usize) {
     panic!("No start position found");
 }
 
-fn count_positions(map: &Vec<Vec<char>>) -> i32 {
-    let mut positions = 0;
+fn find_patrol_positions(map: &Vec<Vec<char>>) -> HashSet<(usize, usize)> {
+    let mut positions = HashSet::new();
     let mut cur_pos = get_start_pos(map);
     let mut visited = vec![vec![false; map[0].len()]; map.len()];
 
     // up, right, down, left
-    let directions: [(i32, i32); 4] = [
-        (0, -1),
-        (1, 0),
-        (0, 1),
-        (-1, 0),
-    ];
+    let directions: [(i32, i32); 4] = [(0, -1), (1, 0), (0, 1), (-1, 0)];
     let mut cur_dir: usize = 0;
 
     loop {
@@ -51,7 +51,7 @@ fn count_positions(map: &Vec<Vec<char>>) -> i32 {
 
         if !visited[y][x] {
             visited[y][x] = true;
-            positions += 1;
+            positions.insert(cur_pos);
         }
 
         if next_x < 0 || next_x >= map[0].len() as i32 || next_y < 0 || next_y >= map.len() as i32 {
@@ -69,4 +69,57 @@ fn count_positions(map: &Vec<Vec<char>>) -> i32 {
     positions
 }
 
+fn detect_loop(map: &Vec<Vec<char>>) -> bool {
+    let mut cur_pos = get_start_pos(map);
+    let mut visited = vec![vec![vec![false; 4]; map[0].len()]; map.len()];
 
+    let directions: [(i32, i32); 4] = [(0, -1), (1, 0), (0, 1), (-1, 0)];
+    let mut cur_dir: usize = 0;
+
+    loop {
+        let (x, y) = cur_pos;
+        let (dx, dy) = directions[cur_dir];
+        let next_x: i32 = x as i32 + dx;
+        let next_y: i32 = y as i32 + dy;
+
+        if !visited[y][x][cur_dir] {
+            visited[y][x][cur_dir] = true;
+        } else {
+            return true;
+        }
+
+        // If we leave the map then we don't have a loop
+        if next_x < 0 || next_x >= map[0].len() as i32 || next_y < 0 || next_y >= map.len() as i32 {
+            return false;
+        }
+
+        if map[next_y as usize][next_x as usize] == '#' {
+            cur_dir = (cur_dir + 1) % 4;
+            continue;
+        }
+
+        cur_pos = (next_x as usize, next_y as usize);
+    }
+
+    false
+}
+
+fn count_possible_loops(map: &Vec<Vec<char>>, positions: &HashSet<(usize, usize)>) -> i32 {
+    let mut loops = 0;
+    let start_pos = get_start_pos(map);
+
+    for pos in positions {
+        // Not allowed to place anything on top of the guard
+        if pos == &start_pos {
+            continue;
+        }
+
+        let mut new_map = map.clone();
+        new_map[pos.1][pos.0] = '#';
+        if detect_loop(&new_map) {
+            loops += 1;
+        }
+    }
+
+    loops
+}
