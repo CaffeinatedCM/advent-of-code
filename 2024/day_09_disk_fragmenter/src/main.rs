f4db01c23038e697ff16185a7e9ac9d060d654ec
Joshua Letcher <caffeinatedcm@gmail.com>
2024-12-10 11:34:57 -0600

    2024 day 9 part 2

--- 2024/day_09_disk_fragmenter/src/main.rs
+++ 2024/day_09_disk_fragmenter/src/main.rs
@@ -1,20 +1,21 @@
 use std::str::FromStr;
 
 fn main() {
-    let args = std::env::args().collect::<Vec<String>>();
+    let input_file = std::env::args().nth(1).expect("Usage: <program> <input file>");
 
-    if args.len() < 2 {
-        println!("Usage: {} <input file>", args[0]);
-        std::process::exit(1);
-    }
-
-    let input_str = std::fs::read_to_string(&args[1]).unwrap().trim().to_string();
-    let mut disk = input_str.parse::<Disk>().unwrap();
-    disk.frag();
-    println!("Checksum after frag: {}", disk.checksum());
+    let input_str = std::fs::read_to_string(input_file)
+        .unwrap()
+        .trim()
+        .to_string();
+    let mut frag_disk = input_str.parse::<Disk>().unwrap();
+    let mut defrag_disk = frag_disk.clone();
+    frag_disk.frag();
+    println!("Checksum after frag: {}", frag_disk.checksum());
+    defrag_disk.defrag();
+    println!("Checksum after defrag: {}", defrag_disk.checksum());
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 struct Disk {
     // Use -1 for empty space, otherwise file_id
     positions: Vec<i32>,
@@ -89,6 +90,56 @@ impl Disk {
                 } else {
                     left_idx += 1;
                 }
+            }
+        }
+    }
+
+    fn defrag(&mut self) {
+        let mut right_idx = self.positions.len() - 1;
+        while right_idx > 0 {
+            let cur_file_id = self.positions[right_idx];
+            if cur_file_id == -1 {
+                right_idx -= 1;
+                continue;
+            }
+
+            // Find current file's length
+            let mut file_len = 1;
+            while (right_idx as i32 - file_len as i32) > 0
+                && self.positions[right_idx - file_len] == cur_file_id
+            {
+                file_len += 1;
+            }
+
+            // Find the next empty space that has enough room
+            let mut left_idx = 0;
+            let mut empty_len = 0;
+            while left_idx < right_idx {
+                if self.positions[left_idx] == -1 {
+                    empty_len = 1;
+                    let mut next_idx = left_idx + 1;
+                    while next_idx < right_idx && self.positions[next_idx] == -1 {
+                        empty_len += 1;
+                        next_idx += 1;
+                    }
+
+                    if empty_len >= file_len {
+                        break;
+                    }
+                }
+
+                left_idx += 1;
+            }
+
+            if left_idx == right_idx || empty_len < file_len {
+                right_idx -= file_len;
+                continue;
+            }
+
+            // Move the file to the empty space
+            for i in 0..file_len {
+                self.positions[left_idx + i] = cur_file_id;
+                self.positions[right_idx - i] = -1;
             }
         }
     }
