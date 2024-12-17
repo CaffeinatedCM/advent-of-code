6b96de1fffab824ed28b77e6a674f63c933b4990
Josh <josh.letcher@skyspecs.com>
2024-12-16 22:33:04 -0600

    2024 day 15 part 2

--- 2024/day_15_warehouse_woes/src/main.rs
+++ 2024/day_15_warehouse_woes/src/main.rs
@@ -11,8 +11,17 @@ fn main() {
     let after_simulation = simulate(&input);
     println!("After simulation:");
     print_map(&after_simulation);
-    let gps = gps(&after_simulation);
-    println!("GPS: {}", gps);
+    let gps_score = gps(&after_simulation);
+    println!("GPS: {}", gps_score);
+    let wide_map_input = make_wide_map(&input);
+    println!("Wide map:");
+    print_map(&wide_map_input.map);
+    println!("Wide map start pos: {:?}", wide_map_input.start_pos);
+    let after_wide_simulation = simulate(&wide_map_input);
+    println!("After wide simulation:");
+    print_map(&after_wide_simulation);
+    let wide_gps = gps(&after_wide_simulation);
+    println!("Wide GPS: {}", wide_gps);
 }
 
 fn print_map(map: &Vec<Vec<char>>) {
@@ -49,7 +58,46 @@ fn parse_input(input: &str) -> Input {
         .flat_map(|line| line.chars())
         .collect::<Vec<char>>();
 
-    Input { map, start_pos, moves }
+    Input {
+        map,
+        start_pos,
+        moves,
+    }
+}
+
+fn make_wide_map(input: &Input) -> Input {
+    let map = &input.map;
+    let mut wide_map = vec![vec![' '; map[0].len() * 2]; map.len()];
+
+    for y in 0..map.len() {
+        for x in 0..map[0].len() {
+            match map[y][x] {
+                '#' => {
+                    wide_map[y][x * 2] = '#';
+                    wide_map[y][x * 2 + 1] = '#';
+                }
+                '.' => {
+                    wide_map[y][x * 2] = '.';
+                    wide_map[y][x * 2 + 1] = '.';
+                }
+                '@' => {
+                    wide_map[y][x * 2] = '@';
+                    wide_map[y][x * 2 + 1] = '.';
+                }
+                'O' => {
+                    wide_map[y][x * 2] = '[';
+                    wide_map[y][x * 2 + 1] = ']';
+                }
+                _ => {}
+            }
+        }
+    }
+
+    Input {
+        map: wide_map,
+        start_pos: (input.start_pos.0 * 2, input.start_pos.1),
+        moves: input.moves.clone(),
+    }
 }
 
 fn simulate(input: &Input) -> Vec<Vec<char>> {
@@ -79,6 +127,42 @@ fn simulate(input: &Input) -> Vec<Vec<char>> {
                     map[pos.1 as usize][pos.0 as usize] = '@';
                 }
             }
+            '[' => {
+                if dy == 0 && push(&mut map, new_pos, (dx, dy)) {
+                    map[pos.1 as usize][pos.0 as usize] = '.';
+                    pos = new_pos;
+                    map[pos.1 as usize][pos.0 as usize] = '@';
+                } else if dy != 0 {
+                    let other_half = (new_pos.0 + 1, new_pos.1);
+                    let mut tmp_map = map.clone();
+                    if push(&mut tmp_map, new_pos, (dx, dy))
+                        && push(&mut tmp_map, other_half, (dx, dy))
+                    {
+                        map = tmp_map;
+                        map[pos.1 as usize][pos.0 as usize] = '.';
+                        pos = new_pos;
+                        map[pos.1 as usize][pos.0 as usize] = '@';
+                    }
+                }
+            }
+            ']' => {
+                if dy == 0 && push(&mut map, new_pos, (dx, dy)) {
+                    map[pos.1 as usize][pos.0 as usize] = '.';
+                    pos = new_pos;
+                    map[pos.1 as usize][pos.0 as usize] = '@';
+                } else if dy != 0 {
+                    let other_half = (new_pos.0 - 1, new_pos.1);
+                    let mut tmp_map = map.clone();
+                    if push(&mut tmp_map, new_pos, (dx, dy))
+                        && push(&mut tmp_map, other_half, (dx, dy))
+                    {
+                        map = tmp_map;
+                        map[pos.1 as usize][pos.0 as usize] = '.';
+                        pos = new_pos;
+                        map[pos.1 as usize][pos.0 as usize] = '@';
+                    }
+                }
+            }
             '#' => {}
             _ => {}
         }
@@ -88,17 +172,76 @@ fn simulate(input: &Input) -> Vec<Vec<char>> {
 }
 
 fn push(map: &mut Vec<Vec<char>>, pos: (i32, i32), dir: (i32, i32)) -> bool {
+    let c = map[pos.1 as usize][pos.0 as usize];
     let new_pos = (pos.0 + dir.0, pos.1 + dir.1);
     match map[new_pos.1 as usize][new_pos.0 as usize] {
         '.' => {
             map[pos.1 as usize][pos.0 as usize] = '.';
-            map[new_pos.1 as usize][new_pos.0 as usize] = 'O';
+            map[new_pos.1 as usize][new_pos.0 as usize] = c;
             true
         }
         'O' => {
             if push(map, new_pos, dir) {
                 map[pos.1 as usize][pos.0 as usize] = '.';
-                map[new_pos.1 as usize][new_pos.0 as usize] = 'O';
+                map[new_pos.1 as usize][new_pos.0 as usize] = c;
+                return true;
+            }
+            false
+        }
+        '[' => {
+            // first, is this my other half?
+            // if so, push it
+            if c == ']' && dir == (-1, 0) {
+                if push(map, new_pos, dir) {
+                    map[pos.1 as usize][pos.0 as usize] = '.';
+                    map[new_pos.1 as usize][new_pos.0 as usize] = c;
+                    return true;
+                }
+            }
+            if dir.1 != 0 {
+                // if I'm vertical, see if we can push both halves
+                let mut tmp_map = map.clone();
+                if push(&mut tmp_map, new_pos, dir)
+                    && push(&mut tmp_map, (new_pos.0 + 1, new_pos.1), dir)
+                {
+                    *map = tmp_map;
+                    map[pos.1 as usize][pos.0 as usize] = '.';
+                    map[new_pos.1 as usize][new_pos.0 as usize] = c;
+                    return true;
+                }
+                return false;
+            }
+            if push(map, new_pos, dir) {
+                map[pos.1 as usize][pos.0 as usize] = '.';
+                map[new_pos.1 as usize][new_pos.0 as usize] = c;
+                return true;
+            }
+            false
+        }
+        ']' => {
+            if c == '[' && dir == (1, 0) {
+                if push(map, new_pos, dir) {
+                    map[pos.1 as usize][pos.0 as usize] = '.';
+                    map[new_pos.1 as usize][new_pos.0 as usize] = c;
+                    return true;
+                }
+            }
+            if dir.1 != 0 {
+                // if I'm vertical, see if we can push both halves
+                let mut tmp_map = map.clone();
+                if push(&mut tmp_map, new_pos, dir)
+                    && push(&mut tmp_map, (new_pos.0 - 1, new_pos.1), dir)
+                {
+                    *map = tmp_map;
+                    map[pos.1 as usize][pos.0 as usize] = '.';
+                    map[new_pos.1 as usize][new_pos.0 as usize] = c;
+                    return true;
+                }
+                return false;
+            }
+            if push(map, new_pos, dir) {
+                map[pos.1 as usize][pos.0 as usize] = '.';
+                map[new_pos.1 as usize][new_pos.0 as usize] = c;
                 return true;
             }
             false
@@ -112,7 +255,7 @@ fn gps(map: &Vec<Vec<char>>) -> usize {
     map.iter()
         .enumerate()
         .flat_map(|(y, row)| row.iter().enumerate().map(move |(x, &c)| (x, y, c)))
-        .filter(|(_, _, c)| *c == 'O')
-        .map(|(x, y, _)| x + 100*y)
+        .filter(|(_, _, c)| *c == 'O' || *c == '[')
+        .map(|(x, y, _)| x + 100 * y)
         .sum()
 }
